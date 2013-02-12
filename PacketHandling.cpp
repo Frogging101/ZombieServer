0ebd96a84266e5daeb812d99ed64607729918d45
Lucas Fryzek <Nercos7@gmail.com>
2013-02-12 17:08:24 -0500

    fix stupid error

--- PacketHandling.cpp
+++ PacketHandling.cpp
@@ -9,12 +9,12 @@ void login(ENetPeer peer){
 			players[i] = Player(0,0,0,i,peer);
 			//TODO send spawn packet to everyone else
 			for(int j=0;j<MAXPLAYERS;j++){
-				if(players[i] != players[j]){
+				if(players[i].id != players[j].id){
 					std::stringstream packetData;
 					packetData << players[j].id << players[j].x << players[j].y
 						<< players[j].z << players[j].yaw;
 					ENetPacket *spawnPacket = enet_packet_create(packetData.str().c_str(),
-							strlen(packetData.str()+1,ENET_PACKET_FLAG_RELIABLE));
+							packetData.str().length()+1,ENET_PACKET_FLAG_RELIABLE);
 
 					enet_peer_send(players[j].peer,0,spawnPacket); 
 				}
